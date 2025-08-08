b5f6714d0ab55a359e58660b8df78b6261d5212d
Yo Han Joo <yohanjoo98@gmail.com>
2025-08-08 23:57:40 +0900

    fix: schema and session not working properly in next app

--- src/db/auth.ts
+++ src/db/auth.ts
@@ -1,12 +1,29 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
+import * as schema from "./auth-schema";
+import { nextCookies } from "better-auth/next-js";
+import { emailOTP } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
+    schema: schema,
   }),
-  emailAndPassword: {
-    enabled: true,
-  },
+  plugins: [
+    nextCookies(),
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        // Implement the sendVerificationOTP method to send the OTP to the user's email address
+        console.log(
+          "✉️ Sending OTP to:",
+          email,
+          "with code:",
+          otp,
+          "type is:",
+          type
+        );
+      },
+    }),
+  ],
 });
